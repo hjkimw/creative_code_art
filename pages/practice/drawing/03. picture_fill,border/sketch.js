44719b9f25a64a53b2892becb2c68921c5b064af
hyungjin kim <hjux07@gmail.com>
2023-06-01 17:30:42 +0900

    [✨feat] drawing/03.picture_fill,border - stroke, fill을 설정하는 메서드들을 실행해서 속성을 설정하면 그 다음에 속성이 변경되기 전까지 모든 도형에 적용된다.

--- pages/practice/drawing/03. picture_fill,border/sketch.js
+++ pages/practice/drawing/03. picture_fill,border/sketch.js
@@ -1,4 +1,29 @@
 new p5((p) => {
-  p.setup = () => {};
-  p.draw = () => {};
+  p.setup = () => {
+    const { innerWidth: w, innerHeight: h } = globalThis;
+
+    p.createCanvas(w, h);
+    p.background(220);
+
+    // stroke 색 설정 (그려지기 이전에 설정해야한다.)
+    p.stroke("blue");
+
+    // stroke 굵기 설정 (그려지기 이전에 설정해야한다.)
+    p.strokeWeight(10);
+
+    // 그림이 그려지기 이전에 색상을 채워줘야한다.
+    // fill("red")메서드 이후에 등장하는 도형은 빨간색
+    p.fill("red");
+    p.rect(100, 100, 100);
+    p.circle(100, 200, 100);
+
+    // fill("blue")메서드 이후에 등장하는 도형은 파랑색
+    p.fill("blue");
+    p.rect(100, 200, 100);
+
+    // noFill()메서드 이후에 등장하는 도형은 빈 색상
+    p.noFill();
+    p.rect(200, 200, 100);
+  };
+  // p.draw = () => {};
 });
