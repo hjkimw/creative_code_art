5b3ff879bf9faf582af8dd2eb72c82cfed607fa7
hyungjin kim <hjux07@gmail.com>
2023-06-01 17:56:10 +0900

    [✨feat] drawing/04.pictrue_color - fill() 메서드의 argument에 String으로 color값을 설정하는게 아닌 RGB[alpha]로 보다 상세한 color를 설정

--- pages/practice/drawing/04. picture_color/sketch.js
+++ pages/practice/drawing/04. picture_color/sketch.js
@@ -1,4 +1,19 @@
 new p5((p) => {
-  p.setup = () => {};
-  p.draw = () => {};
+  p.setup = () => {
+    const { innerWidth: w, innerHeight: h } = globalThis;
+    const squareSize = 100,
+          alpha = 100;
+
+    p.createCanvas(w, h);
+    p.background(220);
+
+    p.rect(100, 100, squareSize);
+
+    // fill(v1, v2, v3, [alpha])
+    p.fill(255, 0, 255, alpha); // magenta color set
+    p.rect(150, 150, squareSize);
+
+  };
+  
+  // p.draw = () => {};
 });
