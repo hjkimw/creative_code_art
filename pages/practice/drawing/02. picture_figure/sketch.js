26043d66bc5a19611d4e6c7a5314c5e48d1fdfd5
hyungjin kim <hjux07@gmail.com>
2023-06-01 15:06:09 +0900

    [✨feat] drawing/02.picture_figure - 포인트 찍기 및 도형 그리기

--- pages/practice/drawing/02. picture_figure/sketch.js
+++ pages/practice/drawing/02. picture_figure/sketch.js
@@ -1,4 +1,23 @@
 new p5((p) => {
-  p.setup = () => {};
-  p.draw = () => {};
+  p.setup = () => {
+    const { innerWidth: w, innerHeight: h } = globalThis;
+
+    p.createCanvas(w, h);
+    p.background(220);
+
+    // point(x, y, [z])
+    // p.point(w / 2, h / 2); // center position
+
+    // rect(x, y, w, [h], [tl], [tr], [br], [bl])
+    // rect(x, y, w, h, [detailX], [detailY])
+
+    // p.rect(100, 100);
+    // p.rect(100, 100, 100, 50);
+    const squareSize = 500,
+      squareSizeHalf = squareSize / 2;
+
+    p.rect(w / 2 - squareSizeHalf, h / 2 - squareSizeHalf, squareSize);
+  };
+
+  // p.draw = () => {};
 });
