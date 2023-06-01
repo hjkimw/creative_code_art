afbf1aa0b77664a69dc9b63aa84f07ea2d81fc84
hyungjin kim <hjux07@gmail.com>
2023-06-01 14:38:20 +0900

    [✨feat] drawing/01.picture_location - 스크린 특정 좌표에 그림 그리기 및 응용 인터랙션

--- pages/practice/drawing/01. picture_location/sketch.js
+++ pages/practice/drawing/01. picture_location/sketch.js
@@ -1,5 +1,57 @@
+// 화면 좌표 학습
+let _p = null;
+
 new p5((p) => {
   console.log(p);
-  p.setup = () => {};
-  p.draw = () => {};
+
+  // parameter를 global variable에 할당
+  _p = p;
+
+  p.setup = () => {
+    const { innerWidth, innerHeight } = globalThis;
+
+    p.createCanvas(innerWidth, innerHeight);
+    p.background(220);
+
+    const circleSize = 100;
+    // circle(x, y, z)
+    // p.circle(0, 0, circleSize);
+    // p.circle(400, 0, circleSize);
+    // p.circle(0, 400, circleSize);
+
+    // top-left
+    p.circle(circleSize, circleSize, circleSize);
+    // middle-left
+    p.circle(circleSize, innerHeight / 2, circleSize);
+    // bottom-left
+    p.circle(innerWidth - circleSize, circleSize, circleSize);
+
+    // top-middle
+    p.circle(innerWidth / 2, circleSize, circleSize);
+    // bottom-middle
+    p.circle(innerWidth / 2, innerHeight - circleSize, circleSize);
+
+    // top-right
+    p.circle(circleSize, innerHeight - circleSize, circleSize);
+    // middle-right
+    p.circle(innerWidth - circleSize, innerHeight / 2, circleSize);
+    // bottom-right
+    p.circle(innerWidth - circleSize, innerHeight - circleSize, circleSize);
+
+    // center
+    p.circle(innerWidth / 2, innerHeight / 2, circleSize * 2);
+
+    // interaction
+    const circleAni = (event) => {
+      window.addEventListener(event, ({ clientX, clientY }) => {
+        p.circle(clientX, clientY, circleSize);
+      });
+    };
+    // circleAni("mousemove");
+    // circleAni("click");
+  };
+
+  // p.draw = () => {};
 });
+
+console.log(_p); // v
