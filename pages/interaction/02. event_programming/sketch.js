435cb8729b6cdccfa4d213bf9e1bb6e386834500
hyungjin kim <hjux07@gmail.com>
2023-06-03 19:44:56 +0900

    [✨feat] interaction/02. event_programming - mouseIsPressed속성으로 마우스가 눌려있는지 확인해서 조건문으로 상호작용하게한다. - mousePressed함수로 마우스를 눌렀을때 함수 실행문을 실행하게 한다.

--- pages/interaction/02. event_programming/sketch.js
+++ pages/interaction/02. event_programming/sketch.js
@@ -1,4 +1,23 @@
 new p5((p) => {
-  p.setup = () => {};
-  p.draw = () => {};
+
+  p.setup = () => {
+    p.createCanvas(p.windowWidth, p.windowHeight);
+    p.background(220);
+  };
+
+  p.draw = () => {
+    // mouseIsPressed속성으로 마우스가 눌려있는지 확인
+    console.log(p.mouseIsPressed);
+
+    if (p.mouseIsPressed) {
+      // p.background(220);
+      p.circle(p.mouseX + p.random(-10, 10), p.mouseY + p.random(-10, 10), p.random(100));
+    }
+  };
+
+  // mousePress function으로 마우스가 눌릴때 함수 실행문을 실행한다.
+  p.mousePressed = () => {
+    p.background(p.random(250), p.random(250), p.random(250));
+  };
+
 });
